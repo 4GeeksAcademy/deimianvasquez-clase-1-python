2fbda06abafa42c6932dd2ce7c935a798d695d3b
Deimian Vásquez <deimianvasquez@gmail.com>
2024-11-20 21:58:48 +0000

    Add class one

---
+++ app.py
@@ -0,0 +1,19 @@
+# name = "Juan"
+# last_name = "Peréz"
+
+# print(f"{name} {last_name} dijo \"chao\"")
+
+# suma = lambda item: print(f"Hola {item}")
+
+# suma("deimian")
+
+
+# for index in range(100, -1, -1):
+#     print(index)
+
+names = ["Deimian", "José", "Mario", "Cristopher", "Jesus"]
+print(len(names))
+names.append("Ricardo")
+# imprimir ultimo de la una lista
+print(names[len(names)-1])
+print(names[-1])
